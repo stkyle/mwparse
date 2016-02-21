14f4133ce5858f667970d3ce1323d5fb64756c38
stkyle <stkyle@users.noreply.github.com>
2016-02-21 16:18:54 -0500

    Update authenticode.py

--- authenticode.py
+++ authenticode.py
@@ -24,9 +24,36 @@ ContentType ::= OBJECT IDENTIFIER
 
 SignerInfos ::= SET OF SignerInfo
 """
-signeddata = 
-from pyasn1.type import univ, namedtype, tag
-class SignedData(univ.Sequence):
-    Version = namedtype.NamedTypes(namedtype.NamedType('version', univ.Integer())
-    digestAlgorithms
+from pyasn1.type import tag,namedtype,namedval,univ,constraint,char,useful
+from pyasn1_modules import rfc2315
+from pyasn1_modules.rfc2315 import *
+from pyasn1_modules.rfc2315 import SignedData, Version
+from pyasn1.type import univ
+from pyasn1.codec.der import decoder as der_decoder
+from pyasn1 import debug
+# debug.setLogger(debug.Debug('all'))
 
+def decode(signature):
+    contentInfo, _ = der_decoder.decode(signature, asn1Spec=rfc2315.ContentInfo())
+    contentType = contentInfo.getComponentByName('contentType')
+    
+    contentInfoMap = {
+        (1, 2, 840, 113549, 1, 7, 1): rfc2315.Data(),
+        (1, 2, 840, 113549, 1, 7, 2): rfc2315.SignedData(),
+        (1, 2, 840, 113549, 1, 7, 3): rfc2315.EnvelopedData(),
+        (1, 2, 840, 113549, 1, 7, 4): rfc2315.SignedAndEnvelopedData(),
+        (1, 2, 840, 113549, 1, 7, 5): rfc2315.DigestedData(),
+        (1, 2, 840, 113549, 1, 7, 6): rfc2315.EncryptedData()
+        }
+    
+    content, _ = der_decoder.decode(
+        contentInfo.getComponentByName('content'),
+        asn1Spec=contentInfoMap[contentType]
+        )
+    
+    return content
+
+print content.getComponentByName('version')
+print content.getComponentByName('certificates')
+print content.getComponentByName('digestAlgorithms')
+print content.getComponentByName('contentInfo')
